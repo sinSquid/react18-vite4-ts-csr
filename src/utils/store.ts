ff309b1507e87ef6e1ed7fc9e955b0af66e04c65
youlongqian <youlongqian@xunlei.com>
2023-05-06 17:59:45 +0800

    feat:eslint调整

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -6,12 +6,10 @@ const createActionTypes = (type = '') => {
   return {
     REQUEST: `${t}_REQUEST_${id}`,
     SUCCESS: `${t}_SUCCESS_${id}`,
-    FAILURE: `${t}_FAILURE_${id}`,
+    FAILURE: `${t}_FAILURE_${id}`
   }
 }
 
-const createActionType = (type = '') => {
-  return `${type.toUpperCase()}_${uuid()}`
-}
+const createActionType = (type = '') => `${type.toUpperCase()}_${uuid()}`
 
 export { createActionTypes, createActionType }
