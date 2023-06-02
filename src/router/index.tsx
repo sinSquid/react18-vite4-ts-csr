1716cea2092ef092b8c3dd08a67137623d66cdda
youlongqian <youlongqian@xunlei.com>
2023-06-02 18:01:29 +0800

    feat:路由懒加载优化

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,43 +1,41 @@
 import React, { lazy, Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
 import Home from '#@/views/Home'
-import About from '#@/views/About'
-import Goddess from '#@/views/Goddess'
-import packRouteLazy from '#@/utils/packRouteLazy'
 
-const RobotLazy = lazy(() => import('#@/views/Robot'))
+const modules: { [key: string]: React.LazyExoticComponent<() => JSX.Element> } = {
+  '/about': lazy(() => import('#@/views/About')),
+  '/goddess': lazy(() => import('#@/views/Goddess')),
+  '/robot': lazy(() => import('#@/views/Robot'))
+}
 
-function func1(path: string) {
-  return lazy(() => import(`#@/views/${path}`))
-}
-const ToT1 = func1('Robot')
-function func2() {
-  return lazy(() => import('#@/views/Robot'))
-}
-const ToT2 = func2()
+const paths = [
+  {
+    key: '/about'
+  },
+  {
+    key: '/goddess'
+  },
+  {
+    key: '/robot'
+  }
+]
 
 export default function Router() {
-  const Robot = packRouteLazy('#@/views/Robot')
   return useRoutes([
     {
       path: '/',
       element: <Home />
     },
-    {
-      path: '/about',
-      element: <About />
-    },
-    {
-      path: '/goddess',
-      element: <Goddess />
-    },
-    {
-      path: '/robot',
-      element: (
-        <Suspense fallback={<span>lalala</span>}>
-          <ToT1 />
-        </Suspense>
-      )
-    }
+    ...paths.map((e) => {
+      const Com = modules[e.key]
+      return {
+        path: e.key,
+        element: (
+          <Suspense fallback={<span>loading...</span>}>
+            <Com />
+          </Suspense>
+        )
+      }
+    })
   ])
 }
